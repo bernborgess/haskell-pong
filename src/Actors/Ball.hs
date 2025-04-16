d01f9ac7f20242bc20472d2a2cf0f946e3d8c4ad
bernborgess <bernborgess@outlook.com>
2025-04-16 11:29:58 -0300

    Apply changes to `ballDraw`

--- src/Actors/Ball.hs
+++ src/Actors/Ball.hs
@@ -20,11 +20,27 @@ newBall =
 
 ballDraw :: GameData -> StateT GameState IO ()
 ballDraw gd = do
-    ball <- gets gameBall
     let renderer = gameRenderer gd
-        V2 ballX ballY = ballPosition ball
-        px = floorFloat ballX :: CInt
-        py = floorFloat ballY :: CInt
-        size = toEnum $ ballSize ball :: CInt
+    ball <- gets gameBall
     SDL.rendererDrawColor renderer SDL.$= ballColor ball
-    SDL.fillRect renderer (Just $ SDL.Rectangle (SDL.P (SDL.V2 px py)) (SDL.V2 size size))
+
+    -- \* Create a SDL_Rect rectangle to visually represent the object:
+    -- The position of the rectangle should be the center of the object
+    -- (not the upper left corner, as originally defined by SDL).
+    -- This will make collision calculations easier.
+
+    -- Get the original position of the object (upper left corner) , the width and height
+    let V2 posX posY = ballPosition ball
+        width = toEnum $ ballSize ball
+        height = toEnum $ ballSize ball
+
+    -- To move the object's position to its center, subtract the original position from the x coordinate
+    -- by half the object's width
+    let px = floorFloat posX - width `div` 2 :: CInt
+    -- and the y coordinate by half the height
+    let py = floorFloat posY - height `div` 2 :: CInt
+    -- Assign the result of these operations as the final position of the created rectangle.
+    -- The height and width of the object do not need to be transformed.
+    let rectangle = SDL.Rectangle (SDL.P (SDL.V2 px py)) (SDL.V2 width height)
+
+    SDL.fillRect renderer (Just rectangle)
